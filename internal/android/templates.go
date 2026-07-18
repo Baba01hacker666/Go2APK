fb3b1fd32fcbf5ec784725f731856feac5287f12
Baba01hacker666 <117832562+Baba01hacker666@users.noreply.github.com>
2026-07-18 12:45:49 +0000

    Fix Android 15 compatibility by extracting native libs and aligning page size to 16kb

--- internal/android/templates.go
+++ internal/android/templates.go
@@ -10,7 +10,7 @@ import (
 func RenderManifest(cfg config.Config) string {
 	return fmt.Sprintf(`<manifest xmlns:android="http://schemas.android.com/apk/res/android">
     <uses-permission android:name="android.permission.INTERNET" />
-    <application android:theme="%s" android:label="%s" android:allowBackup="true" android:supportsRtl="true">
+    <application android:extractNativeLibs="true" android:theme="%s" android:label="%s" android:allowBackup="true" android:supportsRtl="true">
         <activity android:name=".MainActivity" android:exported="true" android:screenOrientation="%s">
             <intent-filter>
                 <action android:name="android.intent.action.MAIN" />
@@ -38,6 +38,12 @@ android {
         targetSdk %d
         versionName '%s'
         versionCode 1
+    }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging true
+        }
     }
 }
 `, cfg.Package, cfg.TargetSDK, cfg.Package, cfg.MinSDK, cfg.TargetSDK, cfg.Version)
